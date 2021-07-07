3f2e10f309b00053fd3a5249607f6467bd6f95e2
NICKN1 <iranildo.junior@outlook.com>
2021-07-07 00:37:54 -0300

    Adicionado outra Controller

---
+++ Curso Arquitetura Backend/Controllers/CursoController.cs
@@ -0,0 +1,28 @@
+﻿using Curso_Arquitetura_Backend.Models.Cursos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Curso_Arquitetura_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CursoController : ControllerBase
+    {
+        [SwaggerResponse(statusCode: 201, description: "Sucesso ao Cadastrar um Curso")]
+        [SwaggerResponse(statusCode: 401, description: "Não autorizado")]
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> Post(CursoViewModelInput cursoViewModelInput)
+        {
+            var codigoUsuario = int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
+            return Created("", cursoViewModelInput);
+        }
+
+    }
+}
---
+++ Curso Arquitetura Backend/Models/Cursos/CursoViewModelOutput.cs
@@ -0,0 +1,9 @@
+﻿namespace Curso_Arquitetura_Backend.Models.Cursos
+{
+    public class CursoViewModelOutput
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Login { get; set; }
+    }
+}
